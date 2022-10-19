c16f45026067a200172e12299f53628a25da6d62
Dendy Sapto A <44661300+xhendpibero@users.noreply.github.com>
2022-10-19 21:31:43 +0700

    Initial Commit

---
+++ src/routes/shop/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	export let data;
+	const { products } = data;
+</script>
+
+<h1>Shop</h1>
+{#each products as product}
+	<h2>
+		<a href="/shop/{product.id}" data-sveltekit-prefetch>
+			{product.title}
+		</a>
+	</h2>
+	<p>{product.description}</p>
+{/each}
